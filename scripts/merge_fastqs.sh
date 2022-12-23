4b2a9cfd582f50824caeff1e7eafd971b7f5cdd2
susana <susanaglezotero@gmail.com>
2022-12-23 15:51:07 +0100

    script merge files

--- scripts/merge_fastqs.sh
+++ scripts/merge_fastqs.sh
@@ -3,3 +3,15 @@
 # stored in the output directory specified by the second argument ($2).
 #
 # The directory containing the samples is indicated by the first argument ($1).
+
+mkdir -p out/merged
+
+input_directory=$1
+output_directory=$2
+sampleid=$3 
+
+echo "Merging files..."
+
+
+cat $1/$3*.fastq.gz > $2/$3.fastq.gz
+
