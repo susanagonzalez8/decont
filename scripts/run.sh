e6e97552ec1cbecc89dcc98a21818f9d2ac67ba0
susana <susanaglezotero@gmail.com>
2022-12-25 16:53:51 +0100

    Index done

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,1 +1,8 @@
 ./download.sh https://bioinformatics.cnio.es/data/courses/decont/contaminants.fasta.gz ../res yes filtro
+
+STAR \
+	--runThreadN 4 \
+	--runMode genomeGenerate \
+	--genomeDir ../res/contaminants_idx \
+	--genomeFastaFiles ../res/contaminants.fasta \
+	--genomeSAindexNbases 9
